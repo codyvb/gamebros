ea1ec1ac4a8a0dca147634134eab51aae912cc0d
codyvb <codybrown@nyu.edu>
2022-07-11 18:59:50 -0600

    sans serrif

--- tailwind.config.js
+++ tailwind.config.js
@@ -121,7 +121,7 @@ module.exports = {
         '4xl': '2560px',
       },
       fontFamily: {
-        pixeloid: ['"pixeloid"', '"sans-serif"'],
+        pixeloid: ['"pixeloid"', 'sans-serif'],
         'press-start': ['"Press Start 2P"'],
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
         headings: [`"${FONT_FAMILY}"`, ...defaultTheme.fontFamily.sans],
